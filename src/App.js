ca3b953f4708fa18384a307528dfb3bee9a22c48
George Heeres <gheeres@gmail.com>
2024-10-24 17:37:32 -0500

    refactor: Switch to vite.js instead of create-react-app. refactor: Use typescript.

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-
-export default function App() {
-  return (
-    <>
-    Around the world... so many countries... so many languages!
-    </>
-  );
-}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  console.log("App()");
+  return (
+    <>
+      <h1>Countries of the World</h1>
+      <p>Lets take a little journey...</p>
+    </>
+  )
+}
